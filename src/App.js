9d55390931a8b310cf5382f31e3678a0c589fcd3
Kacper Kosicki <kosickikacper1@gmail.com>
2023-08-23 09:47:22 +0200

    Add the Header class, Footer and NavBar. Configurate in App.js and connect Bootstrap.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import EditPost from './components/pages/EditPost/EditPost';
 import SinglePost from './components/pages/SinglePost/SinglePost';
 import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
 
 const App = () => {
   return (
     <main>
     <Container>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/add" element={<AddPost />} />
@@ -19,6 +22,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Container>
     </main>
   );
